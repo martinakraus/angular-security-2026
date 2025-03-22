864abd773d592b48fb4f31d69a4ccde7ef8e428a
Martina Kraus <kraus.martina.m@gmail.com>
2025-03-22 21:24:10 +0100

    Add code for Task 20

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { LoginButtonComponent } from "../auth/login-button/login-button.component";
+import { LogoutButtonComponent } from "../auth/logout-button/logout-button.component";
 
 @Component({
-    selector: 'app-navigation',
-    imports: [RouterLink, RouterLinkActive],
-    templateUrl: './navigation.component.html',
-    styleUrl: './navigation.component.scss'
+  selector: "app-navigation",
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    LoginButtonComponent,
+    LogoutButtonComponent,
+  ],
+  templateUrl: "./navigation.component.html",
+  styleUrl: "./navigation.component.scss",
 })
 export class NavigationComponent {}
